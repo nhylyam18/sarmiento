0efa93696e6fe5e10146197dedb8ee896bd4ee2c
nhylyam18 <nhylsarmiento6@gmail.com>
2024-08-02 17:37:25 +0800

    Cute ko ilambos

---
+++ test.py
@@ -0,0 +1,16 @@
+number_seven = 10
+number_eighteen = 23
+
+first_name = "Nhyl May"
+
+char_sample = "n"
+
+people_age = {
+    'Nhyl': 18,
+    'Rixie': 18,
+    'Kerby': 22
+}
+
+total = number_seven + number_eighteen
+
+print('The result is', total)
